f6694a2b30426a54b36e78ebd1906eace24caf1b
felipetsmartins <felipemartins1983@hotmail.com>
2021-04-12 22:01:29 -0300

    Implementada a direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,7 +8,7 @@ class Pessoa:
 
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -18,10 +18,17 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} = olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+class Mutante(Pessoa):
+    olhos = 3
 
 if __name__ == '__main__':
-    felipe = Pessoa(nome='Felipe')
-    fabio = Pessoa(felipe, nome='Fabio')
+    felipe = Mutante(nome='Felipe')
+    fabio = Homem(felipe, nome='Fabio')
     print(Pessoa.cumprimentar(fabio))
     print(id(fabio))
     print(fabio.cumprimentar())
@@ -35,10 +42,17 @@ if __name__ == '__main__':
     del fabio.olhos
     print(fabio.__dict__)
     print(felipe.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(fabio.olhos)
     print(felipe.olhos)
     print(id(Pessoa.olhos), id(fabio.olhos), id(felipe.olhos))
     print(Pessoa.metodo_estatico(), fabio.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), fabio.nome_e_atributos_de_classe())
+    pessoa =Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(felipe, Pessoa))
+    print(isinstance(felipe, Homem))
+    print(felipe.olhos)
+    print(fabio.cumprimentar())
+    print(felipe.cumprimentar())
